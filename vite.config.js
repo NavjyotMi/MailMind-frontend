c8dd360551633302462bf8066e5e4ba2e6cad728
navjyot <navjyot.mishra.1@gmail.com>
2025-04-14 07:31:38 +0530

    deployment bug fix

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  build: {
-    rollupOptions: {
-      external: ["react-redux"], // Add the problematic module here
-    },
-  },
 });
